061ad38e32e04639df6c1ce118165171f4bcce80
Ihor Filimonov <254675014+ihrfv@users.noreply.github.com>
2026-01-16 00:06:46 +0100

    Create server backbone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod server;
+mod http;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
