769732b6d330c5151e203c3e1e28329bab071502
Ihor Filimonov <254675014+ihrfv@users.noreply.github.com>
2026-01-16 01:13:07 +0100

    Add generic request reading mechanism on the specified address

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -9,5 +12,21 @@ impl Server {
 
     pub fn run(&self) {
         println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(error) => println!("[ERROR] Failed to read from the connection: {}", error),
+                    }
+                },
+                Err(error) => println!("[ERROR] Failed to establish a connection: {}", error),
+            }
+        }
     }
 }
