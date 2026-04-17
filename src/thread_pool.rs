e62636b1eac89581cdac2481726c452ded99c95f
Ihor Filimonov <254675014+ihrfv@users.noreply.github.com>
2026-04-18 00:35:41 +0200

    Add a threadpool factory method with a bounded queue size

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,4 +1,4 @@
-use crossbeam_channel::{Receiver, Sender, unbounded};
+use crossbeam_channel::{Receiver, Sender, bounded, unbounded};
 use std::thread::{self, JoinHandle};
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -10,21 +10,22 @@ pub(crate) struct ThreadPool {
 }
 
 impl ThreadPool {
-    /// Create a new ThreadPool.
+    /// Create a new ThreadPool with an UNBOUNDED queue size.
     ///
     /// The size is the number of threads in the pool.
     ///
     /// # Panics
     ///
-    /// The `new` function will panic if the size is zero.
-    pub(crate) fn new(capacity: usize) -> Self {
-        assert!(capacity > 0);
+    /// The `new` function will panic if the threads size is zero.
+    #[allow(dead_code)]
+    pub(crate) fn new(threads: usize) -> Self {
+        assert!(threads > 0);
 
-        let mut workers = Vec::with_capacity(capacity);
+        let mut workers = Vec::with_capacity(threads);
 
         let (tx, rx) = unbounded();
 
-        for i in 0..capacity {
+        for i in 0..threads {
             workers.push(Worker::new(i, rx.clone()));
         }
 
@@ -34,11 +35,43 @@ impl ThreadPool {
         }
     }
 
-    pub(crate) fn execute<F>(&self, f: F)
+    /// Create a new ThreadPool with a bounded queue size.
+    ///
+    /// threads - the number of threads in the pool.
+    /// queue_size - the number of the underlying queue size.
+    ///     if Workers don't process jobs fast enough using this constructor
+    ///     will cause client to be blocked on send
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the threads size or the queue_size is zero.
+    #[allow(dead_code)]
+    pub(crate) fn with_queue_size(threads: usize, queue_size: usize) -> Self {
+        assert!(threads > 0);
+        assert!(queue_size > 0);
+
+        let mut workers = Vec::with_capacity(threads);
+
+        let (tx, rx) = bounded(queue_size);
+
+        for i in 0..threads {
+            workers.push(Worker::new(i, rx.clone()));
+        }
+
+        ThreadPool {
+            workers,
+            sender: Option::Some(tx),
+        }
+    }
+
+    /// If ThreadBool was created with_queue_size then calling this function can block execution
+    /// until some worker will finish its job and will accept the submitted job
+    /// Otherwise, calling this function is non-blocking
+    pub(crate) fn execute<J>(&self, job: J)
     where
-        F: FnOnce() + Send + 'static,
+        J: FnOnce() + Send + 'static,
     {
-        let job = Box::new(f);
+        let job = Box::new(job);
         self.sender.as_ref()
             .expect("Sender should be always present all the way until the ThreadPool drop")
             .send(job)
