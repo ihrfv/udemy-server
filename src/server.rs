da844847d3b9740685382d4a6c8c2cf38bde55ec
Ihor Filimonov <254675014+ihrfv@users.noreply.github.com>
2026-04-17 17:44:56 +0200

    Extract connection handling into a dedicated trait method with a default imlementation

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::io::Read;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response;
@@ -9,6 +9,26 @@ pub trait Handler {
     fn handle_bad_request(&mut self, error: &ParseError) -> Response {
         eprintln!("Failed to parse a request: {}", error);
         Response::new(StatusCode::BadRequest, None)
+    }
+
+    fn handle_connection(&mut self, stream: &mut TcpStream) {
+        // TODO: update buffer to allow requests of arbitrary (reasonable) size
+        let mut buffer = [0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(_bytes_written) => {
+                println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                let response = match Request::try_from(&buffer[..]) {
+                    Ok(request) => self.handle_request(&request),
+                    Err(error) => self.handle_bad_request(&error),
+                };
+                if let Err(error) = response.send(stream) {
+                    eprintln!("Failed to send a response: {error}");
+                }
+            }
+            Err(error) => {
+                eprintln!("Failed to read from the connection: {}", error)
+            }
+        }
     }
 }
 
@@ -21,34 +41,12 @@ impl Server {
         Server { addr }
     }
 
-    pub fn run(&self, mut handler: impl Handler) {
+    pub fn run(&self, handler: &mut impl Handler) {
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
-
         loop {
             match listener.accept() {
-                Ok((mut stream, _)) => {
-                    // TODO: update buffer to allow requests of arbitrary (reasonable) size
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(bytes_written) => {
-                            println!(
-                                "Received a request: {}. Bytes written: {bytes_written}",
-                                String::from_utf8_lossy(&buffer)
-                            );
-                            let response = match Request::try_from(&buffer[..]) {
-                                Ok(request) => handler.handle_request(&request),
-                                Err(error) => handler.handle_bad_request(&error),
-                            };
-                            if let Err(error) = response.send(&mut stream) {
-                                eprintln!("Failed to send a response: {error}");
-                            }
-                        }
-                        Err(error) => {
-                            eprintln!("Failed to read from the connection: {}", error)
-                        }
-                    }
-                }
+                Ok((mut stream, _)) => handler.handle_connection(&mut stream),
                 Err(error) => eprintln!("Failed to establish a connection: {}", error),
             }
         }
