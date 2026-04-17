e272376c21024546cc8fed18c821c106449de0ef
Ihor Filimonov <254675014+ihrfv@users.noreply.github.com>
2026-04-17 13:44:26 +0200

    Implement website handler file loading with input sanitization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
 
 mod http;
@@ -7,5 +8,7 @@ mod website_handler;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    let defalut_public_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(defalut_public_path);
+    server.run(WebsiteHandler::new(public_path));
 }
